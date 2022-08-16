397b949c39969d56e5dfd57bb31bf5325b440a45
sylvainPanneau <fkorangina@gmail.com>
2022-08-17 00:47:52 +0200

    deno in separate repo

--- src/deno/server.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-import router from "./routes.ts";
-const app = new Application();
-const PORT = 3000;
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.listen({ port: PORT });
-console.log(`Server running on port ${PORT}`);
